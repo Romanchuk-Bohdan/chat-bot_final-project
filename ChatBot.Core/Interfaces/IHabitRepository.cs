0b18b2a4806723a0e6a5e3707c13997c4d29b8e5
Романчук Богдан Володимирович <ipz244_rbv@student.ztu.edu.ua>
2026-05-12 22:29:39 +0300

    Add Habit model, interface and JSON repository

---
+++ ChatBot.Core/Interfaces/IHabitRepository.cs
@@ -0,0 +1,12 @@
+﻿using ChatBot.Core.Models;
+
+namespace ChatBot.Core.Interfaces
+{
+    public interface IHabitRepository
+    {
+        IEnumerable<Habit> GetAllHabits();
+        Habit GetHabitById(string id);
+        void SaveHabit(Habit habit);
+        void DeleteHabit(string id);
+    }
+}
---
+++ ChatBot.Core/Models/Habit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ChatBot.Core.Models
+{
+    public class Habit
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int CurrentStreak { get; set; } = 0;
+        public DateTime? LastCompletedDate { get; set; } 
+    }
+}
