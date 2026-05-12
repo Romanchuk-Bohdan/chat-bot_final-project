8c93e16fd563a87c57b9b7dbd8695ba06a8f0e2f
Романчук Богдан Володимирович <ipz244_rbv@student.ztu.edu.ua>
2026-05-12 22:03:03 +0300

    Add User model and JSON repository implementation. Add references between projects.

---
+++ ChatBot.Core/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChatBot.Core.Models
+{
+    public class User
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Username { get; set; }
+        public DateTime RegistrationDate { get; set; } = DateTime.UtcNow;
+        public bool IsActive { get; set; } = true;
+    }
+}
